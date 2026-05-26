f0b61709a41544589abfd06d554cb3ebd4b2e6fc
Sait <saitikiz@gmail.com>
2026-05-26 15:09:24 +0300

    temizlik yapıldı eski kodlar silindi

--- app/Http/Controllers/PronetController.php
+++ app/Http/Controllers/PronetController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Services\PronetServices\PronetClient;
+use App\Services\PronetServices2\PronetClient;
 use Illuminate\Http\Request;
 
 class PronetController
--- config/pronet.php
+++ config/pronet.php
@@ -5,10 +5,6 @@ return [
     'api_user'    => env('PRONET_API_USERNAME', ''),
     'secret'      => env('PRONET_SECRET', ''),
     'verify_ssl'  => env('PRONET_VERIFY_SSL', false),
-    'code'        => '1830',
-    'username'    => 'onurbonus',
-    'password'    => 'Cas23400!!*',
-
     'bonus_history_url'     => env('BONUS_HISTORY_URL', 'http://127.0.0.1:5001'),
     'bonus_history_timeout' => (int) env('BONUS_HISTORY_TIMEOUT', 30),
 ];
